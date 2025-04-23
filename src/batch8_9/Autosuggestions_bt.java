b3778ba7494d74f8b5d5abb344db756fcdd04570
saisowmya <88575337+Saisow@users.noreply.github.com>
2025-04-23 15:25:08 +0530

    Update Autosuggestions_bt.java

--- src/batch8_9/Autosuggestions_bt.java
+++ src/batch8_9/Autosuggestions_bt.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Autosuggestions_bt {
 
-	//Hello good evening
+	//Hello good morning
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		
